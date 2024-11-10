392934d0618b4f46e922cbd0dc0c822ae35013c5
Temiloluwa Adeoti <temiloluwa.adeoti@gfk-cps.com>
2024-11-10 16:04:50 +0100

    adding FSDP

--- stable-diffusion-small/train.py
+++ stable-diffusion-small/train.py
@@ -5,6 +5,7 @@ import pytorch_lightning as pl
 from omegaconf import OmegaConf
 from pytorch_lightning import seed_everything
 from utils import instantiate_object, load_checkpoint, logger
+from pytorch_lightning.strategies import FSDPStrategy
 
 
 def train_model(config_path, ckpt: str):
